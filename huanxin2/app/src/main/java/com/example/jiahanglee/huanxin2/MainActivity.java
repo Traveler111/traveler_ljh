5501d73cababf55bdf8534113c178c498084b058
jiahangLee <1191039404@qq.com>
2016-11-30 15:04:06 +0800

    Revert "add"
    This reverts commit c7d30c2f183d6e11762d32ea1cdef83dc00680e6.

--- huanxin2/app/src/main/java/com/example/jiahanglee/huanxin2/MainActivity.java
+++ huanxin2/app/src/main/java/com/example/jiahanglee/huanxin2/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.jiahanglee.huanxin2;
 
-import android.annotation.TargetApi;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,8 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.example.jiahanglee.huanxin2.runtimepermissions.PermissionsManager;
-import com.example.jiahanglee.huanxin2.runtimepermissions.PermissionsResultAction;
 import com.hyphenate.EMCallBack;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMMessage;
@@ -66,43 +63,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onProgress(int i, String s) {
                 Log.e("didi","退出登录失败");
-            }
-        });
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        EMClient.getInstance().logout(false, new EMCallBack() {
-            @Override
-            public void onSuccess() {
-                startActivity(new Intent(MainActivity.this,login.class));
-                finish();
-            }
-
-            @Override
-            public void onError(int i, String s) {
-
-            }
-
-            @Override
-            public void onProgress(int i, String s) {
-                Log.e("didi","退出登录失败");
-            }
-        });
-    }
-    @TargetApi(23)
-    private void requestPermissions() {
-        PermissionsManager.getInstance().requestAllManifestPermissionsIfNecessary(this, new PermissionsResultAction() {
-            @Override
-            public void onGranted() {
-//				Toast.makeText(MainActivity.this, "All permissions have been granted", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onDenied(String permission) {
-                //Toast.makeText(MainActivity.this, "Permission " + permission + " has been denied", Toast.LENGTH_SHORT).show();
             }
         });
     }
